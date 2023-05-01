36b33ff23f5dd011a9e3965746c7b41a6f308df9
Gregory Albouy <gregoryalbouy75@gmail.com>
2023-05-01 18:14:24 +0200

    Implement CountingButton

---
+++ src/elm/src/CountingButton.elm
@@ -0,0 +1,53 @@
+module CountingButton exposing (main)
+
+import Browser
+import Html exposing (Html, button, text)
+import Html.Events exposing (onClick)
+
+
+
+-- Main
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> ( 0, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    Int
+
+
+
+-- Update
+
+
+type Msg
+    = Increment
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg count =
+    case msg of
+        Increment ->
+            ( count + 1, Cmd.none )
+
+
+
+-- View
+
+
+view : Int -> Html Msg
+view count =
+    button
+        [ onClick Increment ]
+        [ text ("count is " ++ String.fromInt count) ]
